b35f760bbedfc6b0bbc446494c56b7b45fc7b09b
914049118eddy-ops <914049118eddy@gmail.com>
2026-02-24 15:07:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,3 @@ async def api_alerta(alerta: Alerta, bt: BackgroundTasks):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
-
